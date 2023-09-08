ae9768fbb567ea8ace36b0082d7a5d2f5c61b85c
root <root@spotted-innate-front-fin-01.datacrunch.io>
2023-09-08 15:57:44 +0000

    .

--- scripts_math_sharps/train_phase1_legacy_augmented_autoregressive.sh
+++ scripts_math_sharps/train_phase1_legacy_augmented_autoregressive.sh
@@ -1,6 +1,5 @@
 # interval 0
-
-export EPOCHS=10
+export EPOCHS=30
 export INTERVAL=0
 export D=scaffolding_formula
 export MODEL=gpt2
@@ -13,9 +12,9 @@ export CKPTEPOCH=2
 export CKPT=7
 export QMODEL=augmented_math_${D}_cot_${MODEL}_repharvard/checkpoint_${CKPT}_5e-05_gpt2
 export AMODEL=sharps_augmented_model_scaffolding_formula_phase0_inter_fixq_qgpt2_pgpt2_r0_mbottom_e10_fdiagonal_opt_${CKPT}_repharvard_interval${INTERVAL}/checkpoint_${CKPTEPOCH}_5e-05
-export SAVE=sharps_augmented_model_${D}_phase1_inter_F${F}_gpt2_fixed_e${EPOCHS}_legacy_${CKPTEPOCH}_m${MODELSAVE}_try1_${CKPT}_repharvard_interval${INTERVAL}
+export SAVE=autoregressive_sharps_augmented_model_${D}_phase1_inter_F${F}_gpt2_fixed_e${EPOCHS}_legacy_${CKPTEPOCH}_m${MODELSAVE}_try1_${CKPT}_repharvard_interval${INTERVAL}
 mkdir $SAVE
-CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_phase1_fixed_legacy_math_interval.py \
+CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_phase1_fixed_legacy_math_interval_autoregressive.py \
     --train_path data/${FOLDER}/src1_train.txt \
     --val_path data/${FOLDER}/src1_valid.txt \
     --test_path data/${FOLDER}/src1_test.txt \
@@ -30,12 +29,9 @@ CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_
     --save_model $SAVE \
     --compile 0 \
     --interval $INTERVAL \
-    > ${SAVE}/log.train.text.model${MODELSAVE}.folder${FOLDER}.e${EPOCHS}.lr${LR}.${BSZ} 2>&1&
-
-
+    > ${SAVE}/log.train.text.model${MODELSAVE}.folder${FOLDER}.e${EPOCHS}.lr${LR}.${BSZ} 2>&1
 # interval 2
-
-export EPOCHS=10
+export EPOCHS=30
 export INTERVAL=2
 export D=scaffolding_formula
 export MODEL=gpt2
@@ -48,9 +44,9 @@ export CKPTEPOCH=2
 export CKPT=7
 export QMODEL=augmented_math_${D}_cot_${MODEL}_repharvard/checkpoint_${CKPT}_5e-05_gpt2
 export AMODEL=sharps_augmented_model_scaffolding_formula_phase0_inter_fixq_qgpt2_pgpt2_r0_mbottom_e10_fdiagonal_opt_${CKPT}_repharvard_interval${INTERVAL}/checkpoint_${CKPTEPOCH}_5e-05
-export SAVE=sharps_augmented_model_${D}_phase1_inter_F${F}_gpt2_fixed_e${EPOCHS}_legacy_${CKPTEPOCH}_m${MODELSAVE}_try1_${CKPT}_repharvard_interval${INTERVAL}
+export SAVE=autoregressive_sharps_augmented_model_${D}_phase1_inter_F${F}_gpt2_fixed_e${EPOCHS}_legacy_${CKPTEPOCH}_m${MODELSAVE}_try1_${CKPT}_repharvard_interval${INTERVAL}
 mkdir $SAVE
-CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_phase1_fixed_legacy_math_interval.py \
+CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_phase1_fixed_legacy_math_interval_autoregressive.py \
     --train_path data/${FOLDER}/src1_train.txt \
     --val_path data/${FOLDER}/src1_valid.txt \
     --test_path data/${FOLDER}/src1_test.txt \
@@ -65,11 +61,9 @@ CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_
     --save_model $SAVE \
     --compile 0 \
     --interval $INTERVAL \
-    > ${SAVE}/log.train.text.model${MODELSAVE}.folder${FOLDER}.e${EPOCHS}.lr${LR}.${BSZ} 2>&1&
-
-
+    > ${SAVE}/log.train.text.model${MODELSAVE}.folder${FOLDER}.e${EPOCHS}.lr${LR}.${BSZ} 2>&1
 # interval 1
-export EPOCHS=10
+export EPOCHS=30
 export INTERVAL=1
 export D=scaffolding_formula
 export MODEL=gpt2
@@ -82,9 +76,9 @@ export CKPTEPOCH=2
 export CKPT=7
 export QMODEL=augmented_math_${D}_cot_${MODEL}_repharvard/checkpoint_${CKPT}_5e-05_gpt2
 export AMODEL=sharps_augmented_model_scaffolding_formula_phase0_inter_fixq_qgpt2_pgpt2_r0_mbottom_e10_fdiagonal_opt_${CKPT}_repharvard_interval${INTERVAL}/checkpoint_${CKPTEPOCH}_5e-05
-export SAVE=sharps_augmented_model_${D}_phase1_inter_F${F}_gpt2_fixed_e${EPOCHS}_legacy_${CKPTEPOCH}_m${MODELSAVE}_try1_${CKPT}_repharvard_interval${INTERVAL}
+export SAVE=autoregressive_sharps_augmented_model_${D}_phase1_inter_F${F}_gpt2_fixed_e${EPOCHS}_legacy_${CKPTEPOCH}_m${MODELSAVE}_try1_${CKPT}_repharvard_interval${INTERVAL}
 mkdir $SAVE
-CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_phase1_fixed_legacy_math_interval.py \
+CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_phase1_fixed_legacy_math_interval_autoregressive.py \
     --train_path data/${FOLDER}/src1_train.txt \
     --val_path data/${FOLDER}/src1_valid.txt \
     --test_path data/${FOLDER}/src1_test.txt \
@@ -101,7 +95,7 @@ CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_
     --interval $INTERVAL \
     > ${SAVE}/log.train.text.model${MODELSAVE}.folder${FOLDER}.e${EPOCHS}.lr${LR}.${BSZ} 2>&1
 # interval 3
-export EPOCHS=10
+export EPOCHS=30
 export INTERVAL=3
 export D=scaffolding_formula
 export MODEL=gpt2
@@ -114,9 +108,9 @@ export CKPTEPOCH=3
 export CKPT=7
 export QMODEL=augmented_math_${D}_cot_${MODEL}_repharvard/checkpoint_${CKPT}_5e-05_gpt2
 export AMODEL=sharps_augmented_model_scaffolding_formula_phase0_inter_fixq_qgpt2_pgpt2_r0_mbottom_e10_fdiagonal_opt_${CKPT}_repharvard_interval${INTERVAL}/checkpoint_${CKPTEPOCH}_5e-05
-export SAVE=sharps_augmented_model_${D}_phase1_inter_F${F}_gpt2_fixed_e${EPOCHS}_legacy_${CKPTEPOCH}_m${MODELSAVE}_try1_${CKPT}_repharvard_interval${INTERVAL}
+export SAVE=autoregressive_sharps_augmented_model_${D}_phase1_inter_F${F}_gpt2_fixed_e${EPOCHS}_legacy_${CKPTEPOCH}_m${MODELSAVE}_try1_${CKPT}_repharvard_interval${INTERVAL}
 mkdir $SAVE
-CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_phase1_fixed_legacy_math_interval.py \
+CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_phase1_fixed_legacy_math_interval_autoregressive.py \
     --train_path data/${FOLDER}/src1_train.txt \
     --val_path data/${FOLDER}/src1_valid.txt \
     --test_path data/${FOLDER}/src1_test.txt \
@@ -133,7 +127,7 @@ CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_
     --interval $INTERVAL \
     > ${SAVE}/log.train.text.model${MODELSAVE}.folder${FOLDER}.e${EPOCHS}.lr${LR}.${BSZ} 2>&1
 # interval 4
-export EPOCHS=10
+export EPOCHS=30
 export INTERVAL=4
 export D=scaffolding_formula
 export MODEL=gpt2
@@ -146,9 +140,9 @@ export CKPTEPOCH=3
 export CKPT=7
 export QMODEL=augmented_math_${D}_cot_${MODEL}_repharvard/checkpoint_${CKPT}_5e-05_gpt2
 export AMODEL=sharps_augmented_model_scaffolding_formula_phase0_inter_fixq_qgpt2_pgpt2_r0_mbottom_e10_fdiagonal_opt_${CKPT}_repharvard_interval${INTERVAL}/checkpoint_${CKPTEPOCH}_5e-05
-export SAVE=sharps_augmented_model_${D}_phase1_inter_F${F}_gpt2_fixed_e${EPOCHS}_legacy_${CKPTEPOCH}_m${MODELSAVE}_try1_${CKPT}_repharvard_interval${INTERVAL}
+export SAVE=autoregressive_sharps_augmented_model_${D}_phase1_inter_F${F}_gpt2_fixed_e${EPOCHS}_legacy_${CKPTEPOCH}_m${MODELSAVE}_try1_${CKPT}_repharvard_interval${INTERVAL}
 mkdir $SAVE
-CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_phase1_fixed_legacy_math_interval.py \
+CUDA_VISIBLE_DEVICES=0 TOKENIZERS_PARALLELISM=false stdbuf -oL -eL python train_phase1_fixed_legacy_math_interval_autoregressive.py \
     --train_path data/${FOLDER}/src1_train.txt \
     --val_path data/${FOLDER}/src1_valid.txt \
     --test_path data/${FOLDER}/src1_test.txt \
--- scripts_math_sharps/train_phase2_postphase0_augmented_autoregressive.sh
+++ scripts_math_sharps/train_phase2_postphase0_augmented_autoregressive.sh
@@ -4,19 +4,19 @@ export EPOCHS=10
 export D=scaffolding_formula
 export LR=5e-5
 export CKPTEPOCH=2
-export QCKPTEPOCH=2
+export QCKPTEPOCH=9
 export FOLDER=sharps_augmented_math_${D}
 export F=diagonal
 export CKPT=7
 export MODEL=gpt2
 export MODELSAVE="${MODEL////_}"
 export MODEL=sharps_augmented_model_scaffolding_formula_phase0_inter_fixq_qgpt2_pgpt2_r0_mbottom_e10_fdiagonal_opt_${CKPT}_repharvard_interval${INTERVAL}/checkpoint_${CKPTEPOCH}_5e-05
-export QMODEL=sharps_augmented_model_scaffolding_formula_phase1_inter_Fdiagonal_gpt2_fixed_e10_legacy_2_mgpt2_try1_7_repharvard_interval${INTERVAL}/checkpoint_${QCKPTEPOCH}_5e-05
+export QMODEL=autoregressive_sharps_augmented_model_scaffolding_formula_phase1_inter_Fdiagonal_gpt2_fixed_e10_legacy_2_mgpt2_try1_7_repharvard_interval${INTERVAL}/checkpoint_${QCKPTEPOCH}_5e-05
 export BSZ=32
 export A=1
-export SAVE=sharps_augmented_fixed_model_${D}_phase2_inter_pgpt2${CKPTEPOCH}_qgpt2${QCKPTEPOCH}_postphase0_fixq_initmodel_opt_bsz${BSZ}_nolegacy_e${EPOCHS}_m${MODELSAVE}_try1_legacybest_interval${INTERVAL}
+export SAVE=autoregressive_sharps_augmented_fixed_model_${D}_phase2_inter_pgpt2${CKPTEPOCH}_qgpt2${QCKPTEPOCH}_postphase0_fixq_initmodel_opt_bsz${BSZ}_nolegacy_e${EPOCHS}_m${MODELSAVE}_try1_legacybest_interval${INTERVAL}
 mkdir $SAVE
-TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=0 stdbuf -oL -eL python train_phase2_patchmlp_fixed_legacy_math.py \
+TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=0 stdbuf -oL -eL python train_phase2_patchmlp_fixed_legacy_math_autoregressive.py \
     --train_path data/${FOLDER}/src1_train.txt \
     --val_path data/${FOLDER}/src1_valid.txt \
     --test_path data/${FOLDER}/src1_test.txt \
@@ -45,12 +45,12 @@ export CKPT=7
 export MODEL=gpt2
 export MODELSAVE="${MODEL////_}"
 export MODEL=sharps_augmented_model_scaffolding_formula_phase0_inter_fixq_qgpt2_pgpt2_r0_mbottom_e10_fdiagonal_opt_${CKPT}_repharvard_interval${INTERVAL}/checkpoint_${CKPTEPOCH}_5e-05
-export QMODEL=sharps_augmented_model_scaffolding_formula_phase1_inter_Fdiagonal_gpt2_fixed_e10_legacy_2_mgpt2_try1_7_repharvard_interval${INTERVAL}/checkpoint_${QCKPTEPOCH}_5e-05
+export QMODEL=autoregressive_sharps_augmented_model_scaffolding_formula_phase1_inter_Fdiagonal_gpt2_fixed_e10_legacy_2_mgpt2_try1_7_repharvard_interval${INTERVAL}/checkpoint_${QCKPTEPOCH}_5e-05
 export BSZ=32
 export A=1
-export SAVE=sharps_augmented_fixed_model_${D}_phase2_inter_pgpt2${CKPTEPOCH}_qgpt2${QCKPTEPOCH}_postphase0_fixq_initmodel_opt_bsz${BSZ}_nolegacy_e${EPOCHS}_m${MODELSAVE}_try1_legacybest_interval${INTERVAL}
+export SAVE=autoregressive_sharps_augmented_fixed_model_${D}_phase2_inter_pgpt2${CKPTEPOCH}_qgpt2${QCKPTEPOCH}_postphase0_fixq_initmodel_opt_bsz${BSZ}_nolegacy_e${EPOCHS}_m${MODELSAVE}_try1_legacybest_interval${INTERVAL}
 mkdir $SAVE
-TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=0 stdbuf -oL -eL python train_phase2_patchmlp_fixed_legacy_math.py \
+TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=0 stdbuf -oL -eL python train_phase2_patchmlp_fixed_legacy_math_autoregressive.py \
     --train_path data/${FOLDER}/src1_train.txt \
     --val_path data/${FOLDER}/src1_valid.txt \
     --test_path data/${FOLDER}/src1_test.txt \
@@ -79,12 +79,12 @@ export CKPT=7
 export MODEL=gpt2
 export MODELSAVE="${MODEL////_}"
 export MODEL=sharps_augmented_model_scaffolding_formula_phase0_inter_fixq_qgpt2_pgpt2_r0_mbottom_e10_fdiagonal_opt_${CKPT}_repharvard_interval${INTERVAL}/checkpoint_${CKPTEPOCH}_5e-05
-export QMODEL=sharps_augmented_model_scaffolding_formula_phase1_inter_Fdiagonal_gpt2_fixed_e10_legacy_${CKPTEPOCH}_mgpt2_try1_7_repharvard_interval${INTERVAL}/checkpoint_${QCKPTEPOCH}_5e-05
+export QMODEL=autoregressive_sharps_augmented_model_scaffolding_formula_phase1_inter_Fdiagonal_gpt2_fixed_e10_legacy_${CKPTEPOCH}_mgpt2_try1_7_repharvard_interval${INTERVAL}/checkpoint_${QCKPTEPOCH}_5e-05
 export BSZ=32
 export A=1
-export SAVE=sharps_augmented_fixed_model_${D}_phase2_inter_pgpt2${CKPTEPOCH}_qgpt2${QCKPTEPOCH}_postphase0_fixq_initmodel_opt_bsz${BSZ}_nolegacy_e${EPOCHS}_m${MODELSAVE}_try1_legacybest_interval${INTERVAL}
+export SAVE=autoregressive_sharps_augmented_fixed_model_${D}_phase2_inter_pgpt2${CKPTEPOCH}_qgpt2${QCKPTEPOCH}_postphase0_fixq_initmodel_opt_bsz${BSZ}_nolegacy_e${EPOCHS}_m${MODELSAVE}_try1_legacybest_interval${INTERVAL}
 mkdir $SAVE
-TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=0 stdbuf -oL -eL python train_phase2_patchmlp_fixed_legacy_math.py \
+TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=0 stdbuf -oL -eL python train_phase2_patchmlp_fixed_legacy_math_autoregressive.py \
     --train_path data/${FOLDER}/src1_train.txt \
     --val_path data/${FOLDER}/src1_valid.txt \
     --test_path data/${FOLDER}/src1_test.txt \
@@ -111,12 +111,12 @@ export CKPT=7
 export MODEL=gpt2
 export MODELSAVE="${MODEL////_}"
 export MODEL=sharps_augmented_model_scaffolding_formula_phase0_inter_fixq_qgpt2_pgpt2_r0_mbottom_e10_fdiagonal_opt_${CKPT}_repharvard_interval${INTERVAL}/checkpoint_${CKPTEPOCH}_5e-05
-export QMODEL=sharps_augmented_model_scaffolding_formula_phase1_inter_Fdiagonal_gpt2_fixed_e10_legacy_${CKPTEPOCH}_mgpt2_try1_7_repharvard_interval${INTERVAL}/checkpoint_${QCKPTEPOCH}_5e-05
+export QMODEL=autoregressive_sharps_augmented_model_scaffolding_formula_phase1_inter_Fdiagonal_gpt2_fixed_e10_legacy_${CKPTEPOCH}_mgpt2_try1_7_repharvard_interval${INTERVAL}/checkpoint_${QCKPTEPOCH}_5e-05
 export BSZ=32
 export A=1
-export SAVE=sharps_augmented_fixed_model_${D}_phase2_inter_pgpt2${CKPTEPOCH}_qgpt2${QCKPTEPOCH}_postphase0_fixq_initmodel_opt_bsz${BSZ}_nolegacy_e${EPOCHS}_m${MODELSAVE}_try1_legacybest_interval${INTERVAL}
+export SAVE=autoregressive_sharps_augmented_fixed_model_${D}_phase2_inter_pgpt2${CKPTEPOCH}_qgpt2${QCKPTEPOCH}_postphase0_fixq_initmodel_opt_bsz${BSZ}_nolegacy_e${EPOCHS}_m${MODELSAVE}_try1_legacybest_interval${INTERVAL}
 mkdir $SAVE
-TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=0 stdbuf -oL -eL python train_phase2_patchmlp_fixed_legacy_math.py \
+TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=0 stdbuf -oL -eL python train_phase2_patchmlp_fixed_legacy_math_autoregressive.py \
     --train_path data/${FOLDER}/src1_train.txt \
     --val_path data/${FOLDER}/src1_valid.txt \
     --test_path data/${FOLDER}/src1_test.txt \
@@ -143,12 +143,12 @@ export CKPT=7
 export MODEL=gpt2
 export MODELSAVE="${MODEL////_}"
 export MODEL=sharps_augmented_model_scaffolding_formula_phase0_inter_fixq_qgpt2_pgpt2_r0_mbottom_e10_fdiagonal_opt_${CKPT}_repharvard_interval${INTERVAL}/checkpoint_${CKPTEPOCH}_5e-05
-export QMODEL=sharps_augmented_model_scaffolding_formula_phase1_inter_Fdiagonal_gpt2_fixed_e10_legacy_${CKPTEPOCH}_mgpt2_try1_7_repharvard_interval${INTERVAL}/checkpoint_${QCKPTEPOCH}_5e-05
+export QMODEL=autoregressive_sharps_augmented_model_scaffolding_formula_phase1_inter_Fdiagonal_gpt2_fixed_e10_legacy_${CKPTEPOCH}_mgpt2_try1_7_repharvard_interval${INTERVAL}/checkpoint_${QCKPTEPOCH}_5e-05
 export BSZ=32
 export A=1
-export SAVE=sharps_augmented_fixed_model_${D}_phase2_inter_pgpt2${CKPTEPOCH}_qgpt2${QCKPTEPOCH}_postphase0_fixq_initmodel_opt_bsz${BSZ}_nolegacy_e${EPOCHS}_m${MODELSAVE}_try1_legacybest_interval${INTERVAL}
+export SAVE=autoregressive_sharps_augmented_fixed_model_${D}_phase2_inter_pgpt2${CKPTEPOCH}_qgpt2${QCKPTEPOCH}_postphase0_fixq_initmodel_opt_bsz${BSZ}_nolegacy_e${EPOCHS}_m${MODELSAVE}_try1_legacybest_interval${INTERVAL}
 mkdir $SAVE
-TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=0 stdbuf -oL -eL python train_phase2_patchmlp_fixed_legacy_math.py \
+TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=0 stdbuf -oL -eL python train_phase2_patchmlp_fixed_legacy_math_autoregressive.py \
     --train_path data/${FOLDER}/src1_train.txt \
     --val_path data/${FOLDER}/src1_valid.txt \
     --test_path data/${FOLDER}/src1_test.txt \
