34cc13f2e46c8f3bde60c3729ab6e28ffec4195c
Yi Zhang <youralmight@gmail.com>
2024-01-03 05:22:15 +0000

    add compatability for local models and tokenizers

--- src/models/configuration_teacher.py
+++ src/models/configuration_teacher.py
@@ -1,5 +1,5 @@
 from transformers import PretrainedConfig
-
+from os import path
 class TeacherConfig(PretrainedConfig):
     def __init__(
         self,
@@ -9,5 +9,7 @@ class TeacherConfig(PretrainedConfig):
     ):
         self.base_model = base_model
         self.tokenizer_name = tokenizer_name
+        if path.exists(base_model):
+            self.tokenizer_name = base_model
         super().__init__(**kwargs)
 
